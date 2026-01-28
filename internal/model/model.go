6a255bc6429a1aefb94c9c8256a14eb0b61a1d5a
Songpon Imyen <t.songpon@gmail.com>
2026-01-28 17:04:33 +0700

    remove json anotation from model layer

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,19 +1,19 @@
 package model
 
 type Statement struct {
-	CardNumber     string        `json:"card_number"`
-	TotalPayment   float64       `json:"total_payment"`
-	MinimumPayment float64       `json:"minimum_payment"`
-	PaymentDueDate string        `json:"payment_due_date"`
-	CreditLine     float64       `json:"credit_line"`
-	Transactions   []Transaction `json:"transactions"`
+	CardNumber     string
+	TotalPayment   float64
+	MinimumPayment float64
+	PaymentDueDate string
+	CreditLine     float64
+	Transactions   []Transaction
 }
 
 type Transaction struct {
-	TransactionDate string  `json:"transaction_date"`
-	PostingDate     string  `json:"posting_date"`
-	Description     string  `json:"description"`
-	Amount          float64 `json:"amount"`
-	IsInstallment   bool    `json:"is_installment"`
-	InstallmentTerm string  `json:"installment_term"`
+	TransactionDate string
+	PostingDate     string
+	Description     string
+	Amount          float64
+	IsInstallment   bool
+	InstallmentTerm string
 }
--- internal/service/repository.go
+++ internal/service/repository.go
@@ -5,3 +5,11 @@ import "github.com/tsongpon/helios/internal/model"
 type LLMRepository interface {
 	ParseStatement(statementText string) (model.Statement, error)
 }
+
+type StatementRepository interface {
+	Save(statement model.Statement) error
+}
+
+type TransactionRepository interface {
+	Save(transaction []model.Transaction) error
+}
